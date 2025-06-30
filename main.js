115da0b6142b19ba1277dc82edaaa26033b34991
cesar0121 <143618783+cesar0121@users.noreply.github.com>
2025-06-30 08:14:28 -0300

    aula 30/06/2025

---
+++ main.js
@@ -0,0 +1,53 @@
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
+
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
+
+function diminuiTamanho() {
+    if (tamanhoSenha > 1) {
+        tamanhoSenha--;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
+}
+
+function aumentaTamanho() {
+    if (tamanhoSenha < 20){
+        tamanhoSenha++;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
+}
+
+const campoSenha = document.querySelector('#campo-senha');
+const checkbox = document.querySelectorAll('.checkbox');
+
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const letraMinusculas = 'abcdefghijklmnopqrstuvwxyz';
+const numeros = '0123456789';
+const simbolos = '!@%*?';
+
+
+
+geraSenha();
+
+
+function geraSenha(){
+   let alfabeto = '';
+    if (checkbox[1].cheked){
+    
+    let senha = '' ;
+    for(let i=0; i < tamanhoSenha; i++) {
+        let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+        numeroAleatorio = Math.floor(numeroAleatorio);
+        senha = senha + letrasMaiusculas[numeroAleatorio];
+    }
+    
+    
+    
+    campoSenha.value = senha; 
+}
